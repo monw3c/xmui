502b72207a3e808941f96032f017887451890ac0
mon <505038730@qq.com>
2018-01-20 20:59:58 +0800

    :+1:

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -47,7 +47,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true,
-      title: 'xmui - 基于vue2，为公司产品打(zao)造(lun)的(zi)可复用UI组件'
+      title: 'xmui - 基于vue2，可复用UI组件'
     })
   ]
 })
